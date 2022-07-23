e9f21fe895bdf507ba1f049f62bb31a4a37490d0
yujinii59 <71932792+yujinii59@users.noreply.github.com>
2022-07-24 02:17:49 +0900

    [Silver IV] Title: 기타줄, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -0,0 +1,20 @@
+from math import ceil
+
+n, m = map(int, input().split())
+whole_strings = 1000
+each_of_strings = 1000
+for _ in range(m):
+    whole, each = map(int, input().split())
+    whole_strings = min(whole_strings, whole)
+    each_of_strings = min(each_of_strings, each)
+
+if whole_strings < each_of_strings * 6:
+    money = whole_strings * (n // 6)
+    if whole_strings < each_of_strings * (n % 6):
+        money += whole_strings
+    else:
+        money += each_of_strings * (n % 6)
+else:
+    money = each_of_strings * n
+    
+print(money)
