d65d0bd37c9e47e3c9036d70aabe4258231c9872
yujinii59 <71932792+yujinii59@users.noreply.github.com>
2022-07-30 23:41:34 +0900

    [Silver IV] Title: 크림 파스타, Time: 268 ms, Memory: 52336 KB -BaekjoonHub

--- 백준/Silver/25214. 크림 파스타/크림 파스타.py
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.py
@@ -1,15 +1,13 @@
 n = int(input())
-arr = list(map(int, input().split()))
-answer = [0]
-min_prev = arr[0]
-max_prev = arr[0]
-max_diff = 0
-for num in arr[1:]:
-    if num < min_prev:
-        min_prev = num
-        max_prev = num
-    elif num > max_prev:
-        max_prev = num
-        max_diff = max(max_diff, max_prev - min_prev)
-    answer.append(max_diff)
-print(*answer)
+data = list(map(int, input().split()))
+result = []
+min_val = data[0]
+diff = 0
+for num in data:
+    if num < min_val:
+        min_val = num
+    if num - min_val > diff:
+        diff = num - min_val
+    result.append(diff)
+
+print(*result)
