d885194343c01f35c00a045eff327f7707167f9b
yujinii59 <71932792+yujinii59@users.noreply.github.com>
2022-08-03 23:40:18 +0900

    [Silver II] Title: 숫자판 점프, Time: 144 ms, Memory: 115688 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -1,4 +1,4 @@
-def dfs(r, c, num, cnt):
+def dfs(r, c, num, cnt, set_number):
     if cnt == 6:
         set_number.add(num)
     else:
@@ -8,14 +8,15 @@ def dfs(r, c, num, cnt):
             if move_r < 0 or move_r >= 5 or move_c < 0 or move_c >= 5:
                 continue
             
-            dfs(move_r, move_c, num + board[move_r][move_c], cnt + 1)
-
+            set_number = dfs(move_r, move_c, num + board[move_r][move_c], cnt + 1, set_number)
+    return set_number
+            
 board = [input().split() for _ in range(5)]
 set_number = set()
 moves = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
 for i in range(5):
     for j in range(5):
-        dfs(i, j, board[i][j], 1)
+        set_number = dfs(i, j, board[i][j], 1, set_number)
         
 print(len(set_number))
