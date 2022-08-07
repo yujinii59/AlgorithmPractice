6dee25a9691da75dbb45f1ec809fde4d5c606f37
yujinii59 <71932792+yujinii59@users.noreply.github.com>
2022-08-08 00:05:46 +0900

    [Silver IV] Title: 등수 구하기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.py
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -1,27 +1,19 @@
-n, score, p = map(int, input().split())
-if n > 0:
-    rank = sorted(list(map(int, input().split())), reverse=True)
-    if n == p:
-        if score <= rank[-1]:
-            print(-1)
-        else:
-            for i in range(n):
-                if score > rank[i]:
-                    rnk = i + 1
-                    break
-                elif score == rank[i]:
-                    rnk = i + 1
-                    break
-            print(rnk)
-    else:
-        rnk = n + 1
-        for i in range(n):
-            if score > rank[i]:
-                rnk = i + 1
-                break
-            elif score == rank[i]:
-                rnk = i + 1
-                break
-        print(rnk)
-else:
+n, ts, p = map(int, input().split())
+if n == 0:
     print(1)
+else:
+    data = list(map(int, input().split()))
+    data.append(ts)
+    data.sort(reverse=True)
+    
+    idx = data.index(ts)
+    if n < p:
+        print(idx + 1)
+    else:
+        if idx < p:
+            if data[-1] == ts:
+                print(-1)
+            else:
+                print(idx+1)
+        else:
+            print(-1)
