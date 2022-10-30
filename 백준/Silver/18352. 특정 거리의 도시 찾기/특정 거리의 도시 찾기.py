1613547895a522001da75d3534a4c6c27f7ba9b6
yujinii59 <71932792+yujinii59@users.noreply.github.com>
2022-10-30 15:57:05 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 6204 ms, Memory: 187156 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,28 +1,39 @@
-import heapq
+import sys
+from heapq import heappush, heappop
+
+input = lambda: sys.stdin.readline().rstrip()
+
+def dijkstra(graph, start, target, distance):
+    queue = []
+    heappush(queue, (0, start))
+    distance[start] = 0
+
+    while queue:
+        dist, curr = heappop(queue)
+        if distance[curr] < dist:
+            continue
+
+        for node, d in graph[curr]:
+            cost = dist + d
+            if cost < distance[node]:
+                distance[node] = cost
+                heappush(queue, (cost, node))
+
+    result = [i for i, fin_dist in enumerate(distance) if fin_dist == target]
+
+    return result
 
 n, m, k, x = map(int, input().split())
-conn = {i+1:[] for i in range(n)}
-visited = [0] * (n+1)
+INF = int(1e9)
+
+distance = [INF] * (n + 1)
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
-    a, b = map(int, input().split())
-    conn[a].append(b)
-    
-q = [(0, x)]
-k_dist = set()
-while q:
-    dist, city = heapq.heappop(q)
-    if visited[city] == 0:
-        visited[city] = 1
-        if dist == k:
-            k_dist.add(city)
-        elif dist > k:
-            break
+    u, v = map(int, input().split())
+    graph[u].append([v, 1])
 
-        else:
-            for c in conn[city]:
-                heapq.heappush(q, (dist+1, c))
-
-if k_dist:
-    print(*sorted(k_dist), sep='\n')
-else:
+result = dijkstra(graph, x, k, distance)
+if len(result) == 0:
     print(-1)
+else:
+    print(*sorted(result), sep='\n')
