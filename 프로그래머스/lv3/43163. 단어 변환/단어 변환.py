9e363384657dcae391e8a76135b86cb7fbc4a249
yujinii59 <71932792+yujinii59@users.noreply.github.com>
2023-04-20 11:50:06 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,32 +1,3 @@
-from collections import deque
-
 def solution(begin, target, words):
-    if target not in words:
-        return 0
-    length = len(begin)
-    word_cases = {}
-    for i in range(length):
-        word_cases[i] = {}
-        for word in words:
-            cng = word[:i] + word[i+1:]
-            ls = word_cases[i].get(cng, [])
-            ls.append(word)
-            word_cases[i][cng] = ls
-    
-    min_cnt = 0
-    q = deque([(begin, 0, [])])
-    while q:
-        word, cnt, used = q.popleft()
-        if word == target:
-            min_cnt = cnt
-            break
-            
-        for i in range(length):
-            new = word[:i] + word[i+1:]
-            ls = word_cases[i].get(new, [])
-            for new_word in ls:
-                if new_word not in used:
-                    q.append((new_word, cnt+1, used+[new_word]))
-        print(q)
-            
-    return min_cnt
+    answer = 0
+    return answer
